4e36c555f9c6d09b90d4fdbe4f715097a6d76268
Bruno Torres <33846976+BrunoTorresF@users.noreply.github.com>
2018-07-06 11:37:18 -0500

    update app.js

--- specRunner_min_sprint/src/app.js
+++ specRunner_min_sprint/src/app.js
@@ -1,11 +1,7 @@
 // add x and y
 var sum = function (x, y) {
-  if(typeof x !== 'number'){
-    return "Not a Number";
-  }
   return x + y;
 };
 
-var multiplication = function (x, y){
-  return x * y;
-}
+// multiply y from x
+// var multiply = ...
