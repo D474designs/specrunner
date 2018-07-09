d2335483aba36ed7593dbc489f4fd2d6463177ad
D474designs <jcazares117@gmail.com>
2018-07-08 23:05:18 -0500

    add ES6 fat arrow function expressions

--- specRunner_min_sprint/src/app.js
+++ specRunner_min_sprint/src/app.js
@@ -1,5 +1,5 @@
 // add x and y
-var sum = function (x, y) {
+var sum = (x, y) => {
   if (typeof x === 'string') {
   return 'Not a Number';
 } else { return x + y;}
@@ -7,6 +7,6 @@ var sum = function (x, y) {
 
 // multiply y from x
 // var multiply = ...
-var multiply = function (x, y) {
+var multiply = (x, y) => {
   return x * y;
 };
